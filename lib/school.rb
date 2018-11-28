8bb8a266865d5e940b240918adda493ac0326a3e
karen ercoli <karenercoli@gmail.com>
2018-11-28 13:58:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  def initialize(name, roster)
+    @name = name
+    @roster = {}
+  end
+  def name
+    @name
+  end
+
+  def roster
+    @roster
+  end
+end# code here!
